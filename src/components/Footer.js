2176f273b1eb6554425e842133e1a2c6a0ba456b
Farhan Karim <farhankarim507@gmail.com>
2025-02-03 17:36:03 +0500

    formatting fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Footer() {
     return (
         <footer class="text-center py-10 mt-10">
-        <p class="text-gray-500">&copy; 2025 Farhan Karim </p>
-    </footer>
+            <p class="text-gray-500">&copy; 2025 Farhan Karim </p>
+        </footer>
     );
 }
 
